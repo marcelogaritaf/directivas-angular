8e2231d5c68deb5ffdbccfe8b10ea698f1451c8f
Marcelo Garita <mgaritaf@outlook.com>
2023-09-04 16:14:53 -0600

    else in ngif and ngfor functionallity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,10 +6,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'registro de usuarios';
-  mensaje= " "
-  registrado=false
-  nombre=""
-  apellido=""
+  mensaje= " ";
+  registrado=false;
+  nombre="";
+  apellido="";
+  entradas:any[];//esto por motivo de que no reconoce el object[]
+  //lo que hace esta variable es almanacenar una matriz de objetos
+
+  constructor(){//contructor que posee el array de objetos de todas las entradas 
+    this.entradas=[
+      {titulo:'java perdiento popularidad'},
+      {titulo:'python mucha popularidad'},
+      {titulo:'javascript versatil'},
+      {titulo:'ruby futuro en web 3'}
+    ]
+  }
   registrarUsuario(){
     this.registrado=true
     this.mensaje="usuario registrado con exito"
